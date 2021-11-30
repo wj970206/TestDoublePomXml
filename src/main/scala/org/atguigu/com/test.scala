6b4e8a2ea4dd5ec6a81eac065a92ee47b17981c8
dengweijian <892219254@qq.com>
2021-11-30 22:52:41 +0800

    testDoublePom

---
+++ src/main/scala/org/atguigu/com/test.scala
@@ -0,0 +1,13 @@
+package org.atguigu.com
+
+/**
+ * @Auther:weijian
+ * @Date:2021/11/30-11-30-22:31
+ * @Description:org.atguigu.com
+ * @version:1.0
+ */
+object test {
+  def main(args: Array[String]): Unit = {
+    println("haha")
+  }
+}
